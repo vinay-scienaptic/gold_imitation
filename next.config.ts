ea76f41ac0f25980f6fe726ced68e9195195c7be
PJ1016 <41801479+PJ1016@users.noreply.github.com>
2024-12-18 19:31:27 +0530

    add vercel.json

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true, // Optional: Helps identify potential issues
+  pageExtensions: ["ts", "tsx", "js", "jsx"], // Supports TypeScript and JavaScript pages
+  webpack: (config) => {
+    // Customize Webpack config if necessary
+    return config;
+  },
 };
 
 export default nextConfig;
