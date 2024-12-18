d69ad7794937598313ee1e1171744cbef0981ae7
PJ1016 <41801479+PJ1016@users.noreply.github.com>
2024-12-18 19:46:41 +0530

    local fix

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,6 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  reactStrictMode: true, // Optional: Helps identify potential issues
-  pageExtensions: ["ts", "tsx", "js", "jsx"], // Supports TypeScript and JavaScript pages
-  webpack: (config) => {
-    // Customize Webpack config if necessary
-    return config;
-  },
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  /* config options here */
 };
 
-export default nextConfig;
+module.exports = nextConfig;
